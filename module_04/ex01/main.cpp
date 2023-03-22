29af87991ed0c0344d2129cdf3b678acb49b2607
Pedro Paulo <pe.pn03@gmail.com>
2023-03-22 11:33:54 -0300

    Fix dog copy constructor and implement more tests

--- module_04/ex01/main.cpp
+++ module_04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d <ppaulo-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/17 12:13:32 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2023/03/21 11:57:05 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2023/03/22 11:33:33 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ int	main(void) {
 	}
 
 	std::cout << std::endl << "==== End of subject tests ====" << std::endl;
-	Dog	dog = Dog();
 	Cat	cat = Cat();
 	cat.setBrainIdea("Jelly", 0);
 	cat.setBrainIdea("Jelly1", 1);
@@ -41,5 +40,26 @@ int	main(void) {
 	cat.setBrainIdea("Pasta", 0);
 	std::cout << "cat after the change: " << cat.getBrainIdea(0) << std::endl;
 	std::cout << "cat_2: " << cat_2.getBrainIdea(0) << std::endl;
+	
 	std::cout << std::endl;
+
+	Dog	dog = Dog();
+	dog.setBrainIdea("Jelly", 0);
+	dog.setBrainIdea("Jelly1", 1);
+	dog.setBrainIdea("Jelly2", 2);
+	dog.setBrainIdea("Jelly3", 3);
+	Dog dog_2 = Dog(dog);
+	Dog dog_3 = Dog();
+	Dog	dog_4;
+	dog_3 = dog;
+	dog_4 = dog;
+
+	std::cout << "dog_3 first idea: " << dog_3.getBrainIdea(0) << std::endl;
+	std::cout << "dog_4 first idea: " << dog_4.getBrainIdea(0) << std::endl;
+	std::cout << std::endl;
+	std::cout << "dog before the change: " << dog.getBrainIdea(0) << std::endl;
+	dog.setBrainIdea("Pasta", 0);
+	std::cout << "dog after the change: " << dog.getBrainIdea(0) << std::endl;
+	std::cout << "dog_2: " << dog_2.getBrainIdea(0) << std::endl;
+	std::cout << "Teste" << std::endl;
 }
