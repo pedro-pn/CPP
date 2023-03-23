d3e7d2a3a5236e09cce92085fff3067227d82c54
Pedro Paulo <pe.pn03@gmail.com>
2023-03-23 20:22:24 -0300

    Replace indentation spaces with tabs

--- module_05/ex01/Form.hpp
+++ module_05/ex01/Form.hpp
@@ -6,7 +6,7 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 19:50:00 by pedro             #+#    #+#             */
-/*   Updated: 2023/03/22 20:40:48 by pedro            ###   ########.fr       */
+/*   Updated: 2023/03/23 20:20:44 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,48 @@
 # include <string>
 # include "Bureaucrat.hpp"
 
-typedef class Bureaucrat Bureaucrat;
+typedef class Bureaucrat	Bureaucrat;
 
 class Form {
-    
-    public:
+	
+	public:
 
-        class GradeTooHighException : std::exception {
+		class GradeTooHighException : std::exception {
 
-            public:
+			public:
 
-                const char  *what() const throw();
-        };
+				const char  *what() const throw();
+		};
 
-        class GradeTooLowException : std::exception {
+		class GradeTooLowException : std::exception {
 
-            public:
+			public:
 
-                const char  *what() const throw();
-        };
+				const char  *what() const throw();
+		};
 
-        Form(void);
-        Form(std::string const &name, int const gradeToSign, int const gradeToExecute);
-        Form(Form const &rhs);
-        ~Form(void);
+		Form(void);
+		Form(std::string const &name, int const gradeToSign, int const gradeToExecute);
+		Form(Form const &rhs);
+		~Form(void);
 
-        Form&   operator=(Form const &rhs);
+		Form&   operator=(Form const &rhs);
 
-        std::string const   &getName(void) const;
-        bool                getSigned(void) const;
-        int                 getGradeToSign(void) const;
-        int                 getGradeToExecute(void) const;
+		std::string const	&getName(void) const;
+		bool				getSigned(void) const;
+		int					getGradeToSign(void) const;
+		int					getGradeToExecute(void) const;
 
-        void                beSigned(Bureaucrat const &bureaucrat);
+		void				beSigned(Bureaucrat const &bureaucrat);
 
-    private:
+	private:
 
-        std::string const   _name;
-        bool                _signed;
-        int const           _gradeToSign;
-        int const           _gradeToExecute;
-        
+		std::string const	_name;
+		bool				_signed;
+		int const			_gradeToSign;
+		int const			_gradeToExecute;
 };
 
-std::ostream&   operator<<(std::ostream &o, Form const &rhs);
+std::ostream&	operator<<(std::ostream &o, Form const &rhs);
 
 #endif
--- module_05/ex01/main.cpp
+++ module_05/ex01/main.cpp
@@ -6,80 +6,77 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/22 16:21:18 by pedro             #+#    #+#             */
-/*   Updated: 2023/03/22 20:51:24 by pedro            ###   ########.fr       */
+/*   Updated: 2023/03/23 20:21:32 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
 int main(void) {
-    try {
-        Bureaucrat bu = Bureaucrat("Robert", 200);
-          std::cout << bu << std::endl;
-    } catch (Bureaucrat::GradeTooLowException &e) {
-        std::cout << "Couldn't create bureaucrat" << std::endl;
-    }
-    try {
-        Bureaucrat bu = Bureaucrat("Robert", -50);
-          std::cout << bu << std::endl;
-    } catch (Bureaucrat::GradeTooHighException &e) {
-        std::cout << "Couldn't create bureaucrat" << std::endl;
-    }
-    try {
-        Bureaucrat bu = Bureaucrat("Robert", 23);
-          std::cout << bu << std::endl;
-    } catch (Bureaucrat::GradeTooLowException &e) {
-        std::cout << "Couldn't create bureaucrat" << std::endl;
-    } catch (Bureaucrat::GradeTooHighException &e) {
-        std::cout << "Couldn't create bureaucrat" << std::endl;
-    }
+	try {
+		Bureaucrat bu = Bureaucrat("Robert", 200);
+		std::cout << bu << std::endl;
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Couldn't create bureaucrat" << std::endl;
+	}
+	try {
+		Bureaucrat bu = Bureaucrat("Robert", -50);
+		std::cout << bu << std::endl;
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Couldn't create bureaucrat" << std::endl;
+	}
+	try {
+		Bureaucrat bu = Bureaucrat("Robert", 23);
+		std::cout << bu << std::endl;
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Couldn't create bureaucrat" << std::endl;
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Couldn't create bureaucrat" << std::endl;
+	}
 
-    Bureaucrat buLow = Bureaucrat("Nino", 150);
-    Bureaucrat buHigh = Bureaucrat("Faísca", 1);
+	Bureaucrat	buLow = Bureaucrat("Nino", 150);
+	Bureaucrat	buHigh = Bureaucrat("Faísca", 1);
 
-    try {
-     buLow.decrementGrade(1);
-    } catch (Bureaucrat::GradeTooLowException &e) {
-        std::cout << "Cannot decrement grade" << std::endl;
-    }
-    try{
-        buHigh.incrementGrade(1);
-    }   catch (Bureaucrat::GradeTooHighException &e) {
-        std::cout << "Cannot increment grade" << std::endl;
-    }
+	try {
+	 buLow.decrementGrade(1);
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Cannot decrement grade" << std::endl;
+	}
+	try{
+		buHigh.incrementGrade(1);
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Cannot increment grade" << std::endl;
+	}
 
-    try {
-     buLow.incrementGrade(1);
-    } catch (Bureaucrat::GradeTooLowException &e) {
-        std::cout << "Cannot decrement grade" << std::endl;
-    }  catch (Bureaucrat::GradeTooHighException &e) {
-        std::cout << "Cannot increment grade" << std::endl;
-    }
+	try {
+	 buLow.incrementGrade(1);
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Cannot decrement grade" << std::endl;
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Cannot increment grade" << std::endl;
+	}
 
-    try{
-        buHigh.decrementGrade(1);
-    }   catch (Bureaucrat::GradeTooHighException &e) {
-        std::cout << "Cannot increment grade" << std::endl;
-    }   catch (Bureaucrat::GradeTooLowException &e) {
-        std::cout << "Cannot decrement grade" << std::endl;
-    }
-    
-    std::cout << buHigh << std::endl;
-    std::cout << buLow << std::endl;
-    
-    Form    nuclearBomb("manhattan_project", 1, 1);
-    Form    freePassport("passport", 20, 20);
+	try{
+		buHigh.decrementGrade(1);
+	} catch (Bureaucrat::GradeTooHighException &e) {
+		std::cout << "Cannot increment grade" << std::endl;
+	} catch (Bureaucrat::GradeTooLowException &e) {
+		std::cout << "Cannot decrement grade" << std::endl;
+	}
+	
+	std::cout << buHigh << std::endl;
+	std::cout << buLow << std::endl;
+	
+	Form	nuclearBomb("manhattan_project", 1, 1);
+	Form	freePassport("passport", 20, 20);
 
-    std::cout << nuclearBomb << std::endl;
-    buHigh.signForm(nuclearBomb);
-    std::cout << freePassport << std::endl;
-    buLow.signForm(freePassport);
-    std::cout << freePassport << std::endl;
-    buHigh.signForm(freePassport);
-    std::cout << freePassport << std::endl;
+	std::cout << nuclearBomb << std::endl;
+	buHigh.signForm(nuclearBomb);
+	std::cout << freePassport << std::endl;
+	buLow.signForm(freePassport);
+	std::cout << freePassport << std::endl;
+	buHigh.signForm(freePassport);
+	std::cout << freePassport << std::endl;
 
-
-    
-
-    return (0);
+	return (0);
 }
