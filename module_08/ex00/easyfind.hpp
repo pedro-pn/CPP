5b16ed451da0970cbda3f44f7d878bd410b3f750
Pedro Paulo <pe.pn03@gmail.com>
2023-04-05 19:43:29 -0300

    Modify ex00 algorithm

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/05 10:32:26 by pedro             #+#    #+#             */
-/*   Updated: 2023/04/05 11:30:21 by pedro            ###   ########.fr       */
+/*   Updated: 2023/04/05 19:40:44 by pedro            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,15 @@
 # define EASYFIND_HPP
 
 # include <stdexcept>
-
-class NotFoundException : public std::exception {
-	
-	public:
-
-		virtual	const char	*what() const throw();
-};
+# include <algorithm>
 
 template <typename T>
-int	easyfind(T container, int n) {
-	typename T::iterator	it;
-	typename T::iterator	ite = container.end();
-	for (it = container.begin(); it != ite; ++it)
-	{
-		if (*it == n)
-			return (n);
-	}
-	throw NotFoundException();
+int	easyfind(T haystack, int n) {
+	
+	typename T::iterator	result = std::find(haystack.begin(), haystack.end(), n);
+	if (result == haystack.end())
+		throw std::runtime_error("Could't find integer inside the container");
+	return (*result);
 }
-
-# include "easyfind.tpp"
 
 #endif /* EASYFIND_HPP */
